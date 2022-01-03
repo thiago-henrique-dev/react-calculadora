4740d54178f88ed36d5872cae0ce53413580a81b
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-03 13:32:07 -0300

    Componente Button #02

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import './Button.css'
 
-export default props => 
-    <button className='button'>{props.label}</button>
+export default props => {
+    return(
+        <button className={`
+        button
+            ${props.operation ? 'operation' : ''}
+            ${props.double ? 'double' : ''}
+            ${props.triple ? 'triple' : ''}
+        `}>
+    
+            {props.label}
+            
+            </button>
+    )
+}
     
