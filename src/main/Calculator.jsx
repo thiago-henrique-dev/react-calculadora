4dd37822fa139f564b9f1880c649bff75349d794
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-04 15:03:29 -0300

    project finished

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -26,30 +26,33 @@ export default class Calculator extends Component{
         this.setState({...initialState})
     }
     
-    setOperation(operation){
-        if (this.state.current === 0){
-            this.setState({ operation, current: 1, clearDisplay: true})
+    setOperation(operation) {
+
+        if (this.state.current === 0) {
+
+            this.setState({ operation, current: 1, clearDisplay: true })
+
         } else {
-            const equals = operation === '='
-            const currentOperation = this.state.operation
+            const equals = operation === '=';
+            const currentOperation = this.state.operation;
 
             const values = [...this.state.values]
-            try{
-                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
-            } catch(e){
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation}  ${values[1]}`)
+            } catch (e) {
                 values[0] = this.state.values[0]
             }
-            
             values[1] = 0
 
             this.setState({
-                displayValues: values[0],
+                displayValue: values[0],
                 operation: equals ? null : operation,
                 current: equals ? 0 : 1,
                 clearDisplay: !equals,
                 values
             })
         }
+
     }
 
     addDigit(n){
@@ -63,13 +66,14 @@ export default class Calculator extends Component{
             this.setState({ displayValue, clearDisplay: false})
 
             if (n !== '.'){
-                const i = this.state.currents
+                const i = this.state.current
                 const newValue = parseFloat(displayValue)
                 const values = [...this.state.values]
                 values[i] = newValue
                 this.setState({
                     values
                 })
+                console.log(values);
             }
 
     }
